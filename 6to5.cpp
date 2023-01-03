2a16b9a7e8b13be08e78e73981758a0820bbef46
BamideleAdewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2023-01-03 19:29:21 +0000

    Add files via upload

---
+++ 6to5.cpp
@@ -0,0 +1,15 @@
+/*
+This program executes the functions defined in 6to5Functions.cpp
+*/
+
+#include <iostream>
+#include <cmath>
+
+// Declare function
+int sixToFive();
+
+// Call function
+int main() {
+    // Print function to screen
+    std::cout << sixToFive() << "\n";
+}
---
+++ 6to5Functions.cpp
@@ -0,0 +1,10 @@
+/*
+This file contains definitions of files in 6to5
+*/
+
+#include <iostream>
+#include <cmath>
+
+int sixToFive() {
+    return pow(6, 5);
+}
